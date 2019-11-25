f19cde652bd31e565ecf4522b1a3b710b0772da2
Julli-Cumming <jullicumming@gmail.com>
2019-11-25 17:22:51 -0300

    codificacao

--- src/test/java/tests/InformaçõesUsuarioTest.java
+++ src/test/java/tests/InformaçõesUsuarioTest.java
@@ -7,39 +7,39 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class InformaçõesUsuarioTest {
     @Test
     public void testAdicionarUmaInformacaoAdicionalDoUsuario(){
         // Abrindo o navegador
         System.setProperty("webdriver.chrome.driver","C:\\Users\\jcumming\\Drivers\\chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
-
-        // Navagando para a pagina de login - colocar o link de acesso do sistema
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         navegador.get("http://www.juliodelima.com.br/taskit");
+        navegador.manage().window().maximize();
 
-        // Clicar no link que possui o texto Sign in
-        navegador.findElement(By.linkText("Sing in")).click();
-        //WebElement linkSignIn = navegador.findElement(By.linkText("Sing in"));
-        //linkSignIn.click();
+        // Clicar no link que possui o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
 
+        // Identificando o formulário Login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
 
-        //clicar no campo com name login que está dentro do formulário de id "signinbox"
-        WebElement formularioSignInbox = navegador.findElement(By.id("signinbox"));
+        // digitar no campo com name "login" que está dentro do formulário "signinbox" o texto "julio0001"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
 
+        // digitar no campo com name "password" que está dentro do formulário "signinbox" o texto "123456"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
 
-
-        //Digitar no campo com name login que está dentro do formulário de id "signinbox" o texto "julio0001"
-
-        //clicar no campo com name password que está dentro do formulário de id "signinbox"
-
-        //Digitar no campo com name password que está dentro do formulário de id "signinbox" o texto "123456"
-
-        //Clicar no link com o texto "SIGN IN"
+        // clicar no link com texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
 
         //Validar que dentro do elemento com class "me" está o texto "Hi, julio"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals("Hi, Julio",textoNoElementoMe);
 
 
-        navegador.find
 
 
        assertEquals(1,1);
